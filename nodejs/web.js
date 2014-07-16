74ba0def15a5ef8af1ddeec58bebf8dcbdd11ddc
ylang <ylang@andrew.cmu.edu>
2014-07-16 00:38:39 -0700

    make the demo work '

--- nodejs/web.js
+++ nodejs/web.js
@@ -8,22 +8,21 @@ var flag = false;
 
 sp.on('open',function() {
   sp.on('data', function(data) {
-    console.log('>>>>>', data);
+    console.log('>>>>>' + data);
   });
 
-  function writeThenWait(duration) {
+  function writeThenWait() {
     console.log('Calling write...');
     flag = !flag;
     var message = flag ? new Buffer('a') : new Buffer('b');
     sp.write(message, function() {
-      console.log('...Waiting', duration, 'milliseconds...');
+      console.log('...Waiting 5000 milliseconds...');
       // Even though write returned, the data may still be in the pipe, and hasn't reached your robot yet.
-      setInterval(writeThenWait, duration);
+      //setInterval(writeThenWait, duration);
     });
   };
 
   // Stuff starts happening here
-  writeThenWait(5000);
-  //writeThenWait(1000);
+  setInterval(writeThenWait, 5000);
 
 });
